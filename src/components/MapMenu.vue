4e8171f185bba66f341f59e63360e14011e02a2b
vpedrota <v.pedrota@unifesp.br>
2023-08-31 17:39:15 -0300

    Fix: enviando alterações

--- src/components/MapMenu.vue
+++ src/components/MapMenu.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="map-menu">
-    <!-- <SearchBar v-model="searchQuery" /> -->
-    
-    <v-card
-    class="mx-auto menu-card"
-    color="grey-lighten-3"
-    max-width="400"
-    >
-    <v-card-text>
-      <v-text-field
-      
-      density="compact"
-      variant="solo"
-      label="Inserir destino"
-      append-inner-icon="mdi-magnify"
-      single-line
-      hide-details
-      
-      ></v-text-field>
-      
-    </v-card-text>
-  </v-card>
-  
-</div>
+
+  </div>
 </template>
 
 <script lang="ts">
@@ -46,19 +25,15 @@ export default {
 </script>
 
 <style>
-.map-menu {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-  z-index: 900;
-  padding: 50px;
-  margin-top: 20px;
-  min-width: 200px;
-}
 
-.menu-card{
-  width: 80vw;
-  padding: 20px;
-  max-width: 300px;
-}
+  .map-menu {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    z-index: 900;
+    padding: 50px;
+    margin-top: 20px;
+    background-color: black;
+  }
+
 </style>
