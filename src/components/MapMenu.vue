a907630bf36f6368734d15421dcba8605255f76a
Victor Pedrota <v.pedrota@unifesp.br>
2023-09-11 00:08:56 -0300

    Fix: ajustando url do servidor

--- src/components/MapMenu.vue
+++ src/components/MapMenu.vue
@@ -155,7 +155,7 @@
       sendRequest(){
         
         const geojson = this.createGeoJSONWithTwoPoints()
-        const url = 'http://localhost:8080/api/v1/modelo_previsao/geojson';
+        const url = 'https://back-end-projeto-alagamentos-upfpc35ezq-uc.a.run.app/api/v1/modelo_previsao/geojson';
         const store = useCoordinateStore();
 
         axios.post(url, geojson)
